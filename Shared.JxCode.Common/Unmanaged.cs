e10c4ff6c9f7f1f23f3a49d5537a3a1e98833af5
JomiXedYu <jomixedyu@outlook.com>
2021-01-08 01:26:56 +0800

    unmanaged

---
+++ Shared.JxCode.Common/Unmanaged.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace JxCode.Unsafe
+{
+    public unsafe class Unmanaged
+    {
+        public static T CreateStructByBytes<T>(byte[] bytes) where T : unmanaged
+        {
+            //申请非托管内存
+            IntPtr h = Marshal.AllocHGlobal(sizeof(T));
+            //把字节数组填充进非托管内存
+            Marshal.Copy(bytes, 0, h, sizeof(T));
+            //非托管内存转换为托管结构体
+            T t = (T)Marshal.PtrToStructure(h, typeof(T));
+            //释放非托管内存
+            Marshal.FreeHGlobal(h);
+            return t;
+        }
+        public static byte[] CreateBytesByStruct<T>(T stru) where T : unmanaged
+        {
+            //申请非托管内存
+            IntPtr h = Marshal.AllocHGlobal(sizeof(T));
+            //把结构体填充进非托管内存
+            Marshal.StructureToPtr(stru, h, false);
+            byte[] bytes = new byte[sizeof(T)];
+            //把非托管内存的数据复制到字节数组中
+            Marshal.Copy(h, bytes, 0, sizeof(T));
+            //释放非托管内存
+            Marshal.FreeHGlobal(h);
+            return bytes;
+        }
+
+        public unsafe static void ZeroMemory(byte* src, long len)
+        {
+            while (len-- > 0)
+            {
+                src[len] = 0;
+            }
+        }
+    }
+}
---
+++ Shared.JxCode.Common/UnmanagedString.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace JxCode.Unsafe
+{
+    public unsafe class UnmanagedString
+    {
+        public static int strlen(byte* str)
+        {
+            int count = 0;
+            while (true)
+            {
+                if (str[count] == 0)
+                    break;
+                count++;
+            }
+            return count;
+        }
+        public static bool strcmp(byte* str1, byte* str2)
+        {
+            int str1Len = strlen(str1);
+            int str2Len = strlen(str2);
+            if (str1Len != str2Len)
+            {
+                return false;
+            }
+            for (int i = 0; i < str1Len; i++)
+            {
+                if (str1[i] != str2[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
